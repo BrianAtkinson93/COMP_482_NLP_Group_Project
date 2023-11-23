9a3a94477b820112ffe9eb02489fa5e45cf8095b
Brian Atkinson <76421912+BrianAtkinson93@users.noreply.github.com>
2023-11-22 20:18:44 -0800

    Adding Doc Strings

--- gui.py
+++ gui.py
@@ -4,7 +4,25 @@ from tkinter import scrolledtext, ttk
 
 
 class ChatGUI:
+    """
+    A class to create and manage the Graphical User Interface for the chat application.
+
+    Attributes:
+        window (tk.Tk): The main window of the application.
+        top_frame (ttk.Frame): The top frame of the window, containing the chat history.
+        bottom_frame (ttk.Frame): The bottom frame of the window, containing input and send button.
+        chat_history (scrolledtext.ScrolledText): The text area to display chat history.
+        user_input (ttk.Entry): The entry widget for user input.
+        send_button (ttk.Button): The button to send messages.
+        loading_label (ttk.Label): A label to indicate processing of messages.
+    """
     def __init__(self, model):
+        """
+        Initializes the ChatGUI with a specified model.
+
+        Args:
+            model (str): The name of the model to be used for chatting.
+        """
         # self.name = uname
         self.window = tk.Tk()
         self.window.title(f"Chat with {model}")
@@ -48,6 +66,9 @@ class ChatGUI:
         self.user_input.focus_set()
 
     def send_message(self):
+        """
+        Retrieves user input and initiates the process of sending a message.
+        """
         user_message = self.get_user_input()
         if user_message:
             self.update_chat_history(f"User: {user_message}\n", 'user')  # Immediate display of user message
@@ -55,25 +76,53 @@ class ChatGUI:
             threading.Thread(target=self.process_message, args=(user_message,)).start()
 
     def set_send_message_action(self, action):
+        """
+        Sets the action to be performed when the send button is clicked or Enter is pressed.
+
+        Args:
+            action (function): The function to be called when a message is sent.
+        """
         self.send_message = action
         self.send_button.config(command=self.send_message)
         self.window.bind('<Return>', lambda event: self.send_message())
 
     def process_message(self, user_message):
+        """
+        Processes the user message and updates the chat history.
+
+        Args:
+            user_message (str): The message entered by the user.
+        """
         # Update chat history in the main thread with the bot's response
         self.window.after(0, lambda: self.update_chat_history(f"Bot: {user_message}\n", 'bot'))
         self.window.after(0, self.loading_label.pack_forget)  # Hide loading indicator
 
     def update_chat_history(self, message, tag) -> None:
+        """
+        Updates the chat history text area with a new message.
+
+        Args:
+            message (str): The message to be added to the chat history.
+            tag (str): The tag ('user' or 'bot') indicating the sender of the message.
+        """
         self.chat_history.configure(state='normal')
         self.chat_history.insert(tk.END, message, tag)
         self.chat_history.configure(state='disabled')
         self.chat_history.see(tk.END)
 
     def get_user_input(self):
+        """
+        Retrieves the message entered by the user in the input field.
+
+        Returns:
+            str: The message entered by the user.
+        """
         message = self.user_input.get()
         self.user_input.delete(0, tk.END)
         return message
 
     def run(self):
+        """
+        Starts the main loop of the application, displaying the GUI.
+        """
         self.window.mainloop()
