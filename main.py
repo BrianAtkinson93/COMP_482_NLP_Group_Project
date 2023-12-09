c91b35815349b6b0f9e5a9eff41d42d1ecfb571f
Mason Leitch <nosamleitch@gmail.com>
2023-12-09 00:41:26 -0800

    Modified code to only support local gigabyte model.

--- main.py
+++ main.py
@@ -5,17 +5,13 @@ from utilities.config import config
 from utilities.utilities import *
 
 # Dictionary of available models
-models = {
-    1: ("nous_hermes", "nous-hermes-llama2-13b.Q4_0.gguf"),
-    2: ("mistral", "mistral-7b-openorca.Q4_0.gguf"),
-    3: ("mistral_2", "mistral-7b-instruct-v0.1.Q4_0.gguf"),
-    4: ("wizard", "wizardlm-13b-v1.2.Q4_0.gguf")
-}
+
+model_name="gigabyte-1k-q4_0.gguf"
 
 
-def main_local(args):
+def main_local():
     """
-    Runs the chat application with a local GPT4All model.
+    Runs the chat application with the local model.
 
     This function initializes the GUI, ensures the selected model exists, and sets up the chat interface.
 
@@ -23,13 +19,13 @@ def main_local(args):
         args (Namespace): Command line arguments passed to the script.
     """
     # Initialize the GUI with the selected model
-    chat_gui = gui.ChatGUI(models[args.model][0])
+    chat_gui = gui.ChatGUI(model_name)
 
     # Download model if it does not exist
-    ensure_models_exist(models[args.model])
+    ensure_model_exists(model_name)
 
     # Initialize the local GPT4All model
-    chat_model = model.ChatModel_gpt4all(model_path='./models', model_name=models[args.model][1])
+    chat_model = model.ChatModel_gpt4all(model_path='.\models', model_name=model_name)
 
     # Function to handle sending messages
     def send_message_wrapper():
@@ -73,19 +69,4 @@ def main_api(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    # Define command-line arguments
-    parser.add_argument('-a-', '--api', default=False, action='store_true',
-                        help='Use this flag to run with the OpenAI API model.')
-    parser.add_argument("-m", '--model', type=int, default=1, choices=[1, 2, 3, 4],
-                        help="Choose a model number for the local GPT4All model.")
-
-    # Parse arguments
-    arguments = parser.parse_args()
-
-    # Run the appropriate main function based on the arguments
-    if arguments.api:
-        main_api(arguments)
-    else:
-        main_local(arguments)
+    main_local()
